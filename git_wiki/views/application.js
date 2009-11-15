a76a342a96abc79fb31d1cb38972a6902a553131
Jared Grippe <jared@deadlyicon.com>
2009-11-14 21:45:40 -0800

    added a page closing confirmation when changes are unsaved

--- git_wiki/views/application.js
+++ git_wiki/views/application.js
@@ -1,5 +1,5 @@
 (function($) {
-  
+
   function initaliseShowPage(){
     function edit(){
       window.location.pathname = window.location.pathname+'/edit';
@@ -13,56 +13,63 @@
         }
       });
   }
-  
+
   function initaliseEditPage(){
     var save_button = $('form#edit .save');
     var textarea    = $('#editor textarea');
     var preview     = $('#preview');
     var saved_value = textarea.val();
-    
+
     textarea.focus();
-    
+
     function updatePreview(){
       $.post("/preview", { 'body': textarea.val() }, function(a,b){
         preview.html(a);
-      });      
+      });
     }
-    
+
+    function pageHasChanges(){
+      return saved_value !== textarea.val();
+    }
+
     function save(){
       $.post($('form#edit').attr('action'), {body:textarea.val()});
       saved_value = textarea.val();
       save_button.attr('disabled',true);
       console.log('SAVED');
     }
-    
+
     function saveAndClose(){
       save_button.removeAttr('disabled');
       save_button.click();
     }
-    
+
     textarea.keyup(function(){
-      if (saved_value == textarea.val()){
-        save_button.attr('disabled',true);
-      }else{
+      if (pageHasChanges()){
         save_button.removeAttr('disabled');
         updatePreview();
-      }
-      
+      }else{
+        save_button.attr('disabled',true);
+      }Ω
     });
-    
-    $(window).keydown(function(event){
-      if (event.keyCode == 83 && event.metaKey){
-        if (event.shiftKey){
-          save();
-        }else{
-          saveAndClose();
+
+    $(window)
+      .keydown(function(event){
+        if (event.keyCode == 83 && event.metaKey){
+          if (event.shiftKey){
+            save();
+          }else{
+            saveAndClose();
+          }
+          return false;
         }
-        return false;
-      }
-    });
-    
+      });
+
+    window.onbeforeunload = function(){
+      return pageHasChanges() ? "You have unsaved changes." : undefined;
+    };
   }
-  
+
 
   $(document).ready(function(){
     var page = $('#page');
